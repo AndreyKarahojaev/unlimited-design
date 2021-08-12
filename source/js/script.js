a984675cc3b99591c7297cea32b1c7683b4a9915
a.karakhodzhaev <karahodschaev@yandex.ru>
2021-08-12 18:15:10 +0400

    Все еще шапка

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,16 @@
-$('.carousel__inner').slick({
-    arrows: false,
-    dots:true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+const navToggle = document.querySelector(".header-toggle");
+const headNav = document.querySelector(".header-nav");
+const themeButton = document.querySelector('.theme-button');
+
+navToggle.addEventListener("click", (function(){
+    headNav.classList.contains(".header-btn") 
+    ? (headNav.classList.remove(".header-btn"),headNav.classList.add(".header-btn--close")) 
+    : (headNav.classList.remove(".header-btn--close"),headNav.classList.add(".header-btn"));
+}));
+
+themeButton.onclick = function() {
+    navToggle.classList.toggle('line-theme');
+    navToggle.classList.toggle('x-theme');
+};
+
+
